32169385a500d5f106d7e53f24e035a99026cb38
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2015-03-19 23:46:17 +0100

    Ejemplo de Flujo de Bytes Caracter a Caracter añadido. Stream Char to Char example added.

---
+++ Java/Java SE/13 FlujoDeBytesCaracter/Main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Escribe un caracter : ");
+		
+		char letra = 'a';
+		int charInt = 0;
+		
+		//-------------------- ENTRADA ---------------------
+		//Definir la entrada
+		InputStream input = System.in;
+		//leer el Stream que hemos recibido
+		InputStreamReader isr = new InputStreamReader(input);
+		//Utilizamos un lector de buffer, para leer con mas comodidad 
+		BufferedReader br = new BufferedReader(isr);
+		//Leer el STREAM de entrada
+		try {
+			charInt = br.read();
+			letra = (char)charInt;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		// ---------- SALIDA ----------------------------------
+		//Definir la Salida
+		OutputStream out = System.out;
+		//definimos un writer de STREAM
+		OutputStreamWriter osw = new OutputStreamWriter(out );
+		//utilizamos un writer de buffers, que nos ayuda a escribir.
+		BufferedWriter bw = new BufferedWriter(osw);
+		//Escribir el STREAM de salida
+		try {
+			bw.write(letra);
+			// Forzamos a que escriba la salida
+			bw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		
+	}
+
+}
