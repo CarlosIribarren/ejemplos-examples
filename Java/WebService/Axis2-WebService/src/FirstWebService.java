19b1171ce799aa4bfb2bfae55b82945aebe46763
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2015-04-07 18:16:15 +0200

    Ejemplo de WebService con Axis2 añadido. WebService with Axis2 example added.

---
+++ Java/WebService/Axis2-WebService/src/FirstWebService.java
@@ -0,0 +1,13 @@
+
+public class FirstWebService {
+	
+	public String saludar(String nombre)
+	{
+		return "Hola " + nombre + "!";
+	}
+	public int sumar(int op1, int op2)
+	{
+		return op1+op2;
+	}
+
+}
