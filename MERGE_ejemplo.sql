645a07ecec5aa80c3cfb8c19cd509abde5ba5709
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2015-11-11 15:42:18 +0100

    Segunda forma de Merge añadida (Mejorado)

--- MERGE_ejemplo.sql
+++ MERGE_ejemplo.sql
@@ -1,9 +1,35 @@
+NOTA : En este ejemplo se muestra como en la Select se seleciona un registro usando la clausula WHERE
+
 MERGE INTO TDE_FAMILIA ANTES
      USING (SELECT 
             '1' FAM_IDFAMILIA,
             'Ocio' FAM_NOMCAS                
               FROM TDE_FAMILIA
               WHERE FAM_IDFAMILIA=1) DESPUES
+        ON (ANTES.FAM_IDFAMILIA = DESPUES.FAM_IDFAMILIA)
+WHEN MATCHED
+THEN
+   UPDATE SET
+      ANTES.FAM_NOMCAS = DESPUES.FAM_NOMCAS
+WHEN NOT MATCHED
+THEN
+   INSERT     (FAM_IDFAMILIA, 
+                FAM_NOMCAS)
+       VALUES (DESPUES.FAM_IDFAMILIA,
+               DESPUES.FAM_NOMCAS);
+
+
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+
+NOTA : En este ejemplo se muestra como en la Select crea un registro con la clausula "FROM DUAL" y se mira 
+si en la tabla esa existe un registro igual (todos los campos iguales)
+
+MERGE INTO TDE_FAMILIA ANTES
+     USING (SELECT 
+            '1' FAM_IDFAMILIA,
+            'Ocio' FAM_NOMCAS                
+              FROM DUAL) DESPUES
         ON (ANTES.FAM_IDFAMILIA = DESPUES.FAM_IDFAMILIA)
 WHEN MATCHED
 THEN
