f7e70d1aef6dd32efb858a9e6b458423b9c38b87
Carlos Iribarren <ciribarren@euro-help.es>
2016-02-04 15:54:25 +0100

    Spring MVC ejemplo añadido

---
+++ Java/Spring/SpringMVC_Ejemplo_0/src/main/java/es/eurohelp/cursos/springmvc/HomeController.java
@@ -0,0 +1,17 @@
+package es.eurohelp.cursos.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Gestiona las peticiones de la página home de la aplicación.
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String home() {
+		System.out.println("HomeController: procesando petición...");
+		return "/WEB-INF/home.jsp";
+	}
+}
