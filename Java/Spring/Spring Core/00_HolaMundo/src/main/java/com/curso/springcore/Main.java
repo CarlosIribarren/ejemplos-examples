ac444f7bdcab504a7a27acae374bd9b4a72f0689
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2016-02-07 18:58:00 +0100

    Ejemplo de Hola mundo con Spring Core añadido.

---
+++ Java/Spring/Spring Core/00_HolaMundo/src/main/java/com/curso/springcore/Main.java
@@ -0,0 +1,22 @@
+package com.curso.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.curso.springcore.beans.Mundo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/curso/springcore/xml/beans.xml");
+		Mundo m1 = (Mundo) applicationContext.getBean("mundo");
+		Mundo m2 = (Mundo) applicationContext.getBean(Mundo.class);
+		System.out.println(m1.getSaludo());
+		System.out.println(m2.getSaludo());
+		
+		
+	}
+
+}
---
+++ Java/Spring/Spring Core/00_HolaMundo/src/main/java/com/curso/springcore/beans/Mundo.java
@@ -0,0 +1,17 @@
+package com.curso.springcore.beans;
+
+public class Mundo {
+	
+	private String saludo;
+
+	public String getSaludo() {
+		return saludo;
+	}
+
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+	
+	
+
+}
