d03b158da1c6258aea274a341fde729ff8095eb3
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2016-02-07 19:06:13 +0100

    Organizando carpeta de resources para el archivo beans.xml.

--- Java/Spring/Spring Core/00_HolaMundo/src/main/java/com/curso/springcore/Main.java
+++ Java/Spring/Spring Core/00_HolaMundo/src/main/java/com/curso/springcore/Main.java
@@ -1,6 +1,7 @@
 package com.curso.springcore;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.curso.springcore.beans.Mundo;
@@ -10,13 +11,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/curso/springcore/xml/beans.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("xml/beans.xml");
+		
 		Mundo m1 = (Mundo) applicationContext.getBean("mundo");
 		Mundo m2 = (Mundo) applicationContext.getBean(Mundo.class);
+		
 		System.out.println(m1.getSaludo());
 		System.out.println(m2.getSaludo());
 		
-		
+		((ConfigurableApplicationContext)applicationContext).close();
 	}
 
 }
