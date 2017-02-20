6cccaed880fb8b6fd3bcf59571ea24c157bc3835
Carlos Iribarren <ciribarren@euro-help.es>
2017-02-20 16:01:20 +0100

    Se añade la configuración de Spring.

---
+++ Java/JUnit/JUnitEjemplos/src/main/java/ejemplos/junit/config/AppConfig.java
@@ -0,0 +1,12 @@
+
+package ejemplos.junit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan("ejemplos.junit")
+@Configuration
+public class AppConfig {
+
+}
