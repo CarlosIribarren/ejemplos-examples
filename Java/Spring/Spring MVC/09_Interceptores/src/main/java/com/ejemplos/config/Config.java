5c684756305a659f56bec7bfa7f5e54bf45f6671
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2017-06-06 09:43:51 +0200

    Se añaden ejemplo con Interceptores de Spring MVC.

---
+++ Java/Spring/Spring MVC/09_Interceptores/src/main/java/com/ejemplos/config/Config.java
@@ -0,0 +1,10 @@
+package com.ejemplos.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan({ "com.ejemplos.*" })
+public class Config {
+}
