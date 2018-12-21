6e0149be524c53e734adc4bc6f3096a49c998aa5
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2018-12-21 10:53:40 +0100

    Se añade ejemplo de Spring MVC AJAX

---
+++ Java/Spring/Spring MVC/04-Ajax/src/main/java/oiasso/systems/config/Config.java
@@ -0,0 +1,10 @@
+package oiasso.systems.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan({"oiasso.systems"})
+public class Config {
+}
