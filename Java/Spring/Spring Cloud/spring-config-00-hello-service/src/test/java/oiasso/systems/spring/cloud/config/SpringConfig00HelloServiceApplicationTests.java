40fd4338d0402db815318fa59b30353712a23de0
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2021-09-21 01:31:09 +0200

    Added spring cloud basics examples

---
+++ Java/Spring/Spring Cloud/spring-config-00-hello-service/src/test/java/oiasso/systems/spring/cloud/config/SpringConfig00HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package oiasso.systems.spring.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfig00HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
