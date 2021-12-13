270c7e7b26bd096fb55140ba034dbc55fcff25d5
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2021-12-13 17:37:52 +0100

    Create Foo.java

---
+++ Java/Test/JUnit/Foo.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Foo {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(Foo .class);
+
+    public void doThat() {
+        LOGGER.info("start");
+        //...
+        LOGGER.info("finish");
+    }
+}
